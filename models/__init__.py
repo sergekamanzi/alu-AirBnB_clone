290342abb0caf257f05a86d7d78d67d3b0121d23
KAMANZI-SERGE187 <sergekmz187@gmail.com>
2024-01-11 10:08:51 -0800

    init

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
-"""__init__ magic method for models directory"""
-from models.engine.file_storage import FileStorage
+#!/usr/bin/env python3
 
+
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
