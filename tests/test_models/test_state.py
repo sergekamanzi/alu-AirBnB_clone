35e89a480441d4c6f71e2ac0cc656a17247afdab
KAMANZI-SERGE187 <sergekmz187@gmail.com>
2024-01-12 10:12:36 -0800

    test-models_run

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""unittest for User class"""
+import unittest
+from models.state import State
+from models.base_model import BaseModel
+
+
+class TestState(unittest.TestCase):
+    """Test cases for User class."""
+
+    def setUp(self):
+        self.testState = State()
+
+    def testState(self):
+        self.assertTrue(issubclass(self.testState.__class__, BaseModel))
+
+    def test_name(self):
+        self.assertIsInstance(self.testState.name, str)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""unittest for User class"""
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    """Test cases for User class."""
+
+    def setUp(self):
+        self.testUser = User()
+
+    def test_user(self):
+        """Test if User class is subclass of BaseModel."""
+        self.assertTrue(issubclass(self.testUser.__class__, BaseModel))
+
+    def test_email(self):
+        """Test email class attribute."""
+        self.assertIsInstance(self.testUser.email, str)
+
+    def test_password(self):
+        self.assertIsInstance(self.testUser.password, str)
+
+    def test_first_name(self):
+        self.assertIsInstance(self.testUser.first_name, str)
+
+    def test_last_name(self):
+        self.assertIsInstance(self.testUser.last_name, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
